9ed27a3d5f2f65347f873f8f8db8e255a935569c
ivvlko <ivankoevbg@gmail.com>
2021-05-08 17:34:34 +0300

    creating script for retraining naive bayes with more checked data

--- applications/hackernews_nlp/api_connector.py
+++ applications/hackernews_nlp/api_connector.py
@@ -6,5 +6,5 @@ def get_latest_news():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     hn = HackerNews()
-    items = hn.get_last(100)
+    items = hn.new_stories(False, 1000)
     return items
